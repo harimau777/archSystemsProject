f3396c374529687beed6665ac6160a85be332a74
David Moneysmith <moneysmith777@gmail.com>
2017-03-03 08:57:50 -0800

    Timestamp now formatted to be more readable

--- client/app/dataPoint.jsx
+++ client/app/dataPoint.jsx
@@ -1,10 +1,15 @@
 const DataPoint = ({key, dataPoint, selectStream}) => {
   return (
     <tr className="dataPoint">
-      <td>{dataPoint.timestamp}</td>
+      <td>{parseTimeStamp(dataPoint.timestamp)}</td>
       <td>{dataPoint.value}</td>
     </tr>
   );
 };
+
+const parseTimeStamp = (timestamp) => {
+  const match = timestamp.match(/^(.+)T((?:\d\d\:){2}\d\d)/);
+  return match[1] + ' ' + match[2];
+}
 
 export {DataPoint};
